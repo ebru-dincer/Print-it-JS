9d14d35e0d2e72024b390f408f0071746b280f82
Ebru Dincer <ebru.dincer16@gmail.com>
2023-10-09 15:29:16 +0200

    Création carrousel en javascript + ajout bullet points sur fichier html

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,64 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		slidesName:"slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		slidesName:"slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		slidesName:"slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		slidesName:"slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let indexSlide = 0;
+
+const image = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+
+const leftArrow = document.querySelector(".arrow_left");
+leftArrow.addEventListener("click", () => {
+	console.log("un clique sur la flèche gauche");
+	indexSlide--
+	if (indexSlide < 0) {
+		indexSlide = 3;
+	}
+	image.setAttribute("src", `./assets/images/slideshow/${slides[indexSlide].slidesName}`);
+	console.log(indexSlide);
+	console.log(slides[indexSlide].slidesName);
+	tagLine.innerHTML = slides[indexSlide].tagLine;
+	console.log(slides[indexSlide].tagLine);
+	changeDots ();
+});
+
+const rightArrow = document.querySelector(".arrow_right");
+rightArrow.addEventListener("click", () => {
+	console.log("un clique sur la flèche droite");
+	indexSlide++
+	if (indexSlide == slides.length) {
+		indexSlide = 0;
+	}
+	image.setAttribute("src", `./assets/images/slideshow/${slides[indexSlide].slidesName}`);
+	console.log(indexSlide);
+	console.log(slides[indexSlide].slidesName);
+	tagLine.innerHTML = slides[indexSlide].tagLine;
+	console.log(slides[indexSlide].tagLine);
+	changeDots ();
+
+});
+
+function changeDots () {
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach (dot => {
+		dot.classList.remove("dot_selected")
+	} );
+	dots[indexSlide].classList.add("dot_selected");
+};
+
+
+
